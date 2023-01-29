75967a05ca121e391bb7f24308e2424b066ac856
karollann <lannkarol@gmail.com>
2023-01-29 21:56:58 +0100

    add heroPhoto preload

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,4 +1,7 @@
 import styles from "./hero.module.scss";
+import heroPhoto from "../../public/heroPhoto.png";
+
+import Image from "next/image";
 
 import { scrollToID } from "../../utils";
 
@@ -9,6 +12,12 @@ export function Hero() {
         <p className={styles.menu__icon}>👋</p>
         <p className={styles.menu__text}>Hi! I&apos;m Karol</p>
       </div>
+      <Image
+        priority={true}
+        src={heroPhoto}
+        alt="Hero Picture"
+        className={styles.hero__image}
+      />
       <h1 className={styles.hero__heading}>Beginning Web Developer</h1>
     </section>
   );
