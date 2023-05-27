ca4a964fdd2af89112d628acf3a46852ed35d2bb
karollann <104844637+karollann@users.noreply.github.com>
2023-05-27 10:54:02 +0000

    Deploying to gh-pages from @ karollann/portfolio@4844cdc40ea26771509889709da08a7344b1b129 🚀

--- _next/static/mgZC0_gMpAWfSBpX7eiDj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bd6f459b224eb1da.css","static/chunks/pages/index-3e447b0fd3914e33.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
